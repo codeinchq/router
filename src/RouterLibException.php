e066d01b04a669f451685e6c261d636d6c1e5f22
Joan Fabrégat <joan@codeinc.fr>
2018-02-23 17:57:28 +0100

    code optim

--- src/RouterLibException.php
+++ src/RouterLibException.php
@@ -21,6 +21,7 @@
 //
 declare(strict_types = 1);
 namespace CodeInc\Router;
+use Throwable;
 
 
 /**
@@ -30,5 +31,15 @@ namespace CodeInc\Router;
  * @author Joan Fabrégat <joan@codeinc.fr>
  */
 class RouterLibException extends \Exception {
-
+	/**
+	 * RouterLibException constructor.
+	 *
+	 * @param string $message
+	 * @param int|null $code
+	 * @param null|Throwable $previous
+	 */
+	public function __construct(string $message, ?int $code = null, ?Throwable $previous = null)
+	{
+		parent::__construct($message, $code ?? 0, $previous);
+	}
 }
