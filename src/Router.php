016e5b65cd90ab86230745567e9f3813e2f2cca3
Joan Fabrégat <joan@codeinc.fr>
2018-03-14 12:40:34 +0100

    code improvement

--- src/Router.php
+++ src/Router.php
@@ -29,7 +29,6 @@ use CodeInc\Router\Instantiators\DefaultInstantiator;
 use CodeInc\Router\Instantiators\InstantiatorInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use Psr\Http\Server\MiddlewareInterface;
 
 
 /**
