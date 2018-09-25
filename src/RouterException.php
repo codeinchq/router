50fc941f1d0ca2f455567924734e8457d0125f85
Joan Fabrégat <joan@codeinc.fr>
2018-09-25 17:08:59 +0200

    cleanup

--- src/RouterException.php
+++ src/RouterException.php
@@ -35,14 +35,9 @@ class RouterException extends \Exception
 {
     public const CODE_DUPLICATE_ROUTE = 1;
     public const CODE_EMPTY_URI = 2;
-    public const CODE_NOT_A_REQUEST_HANDLER = 3;
-    public const CODE_NOT_A_ROUTABLE_REQUEST_HANDLER = 4;
+    public const CODE_EMPTY_REQUEST_HANDLER_NAMESPACE = 3;
+    public const CODE_NOT_A_REQUEST_HANDLER = 4;
     public const CODE_NOT_WITHIN_NAMESPACE = 5;
-    public const CODE_NO_ROUTE_FOUND = 6;
-    public const CODE_NOT_A_ROUTER = 7;
-    public const CODE_NOT_A_MIDDLEWARE = 8;
-    public const CODE_EMPTY_ROUTE = 9;
-    public const CODE_INVALID_ROUTE = 9;
 
     /**
      * @param string $route
@@ -69,7 +64,7 @@ class RouterException extends \Exception
     public static function emptyRequestHandlersNamespace():self
     {
         return new self("The dynamic router's request handler namespace can not be empty.",
-            self::CODE_EMPTY_URI);
+            self::CODE_EMPTY_REQUEST_HANDLER_NAMESPACE);
     }
 
     /**
@@ -91,59 +86,7 @@ class RouterException extends \Exception
     public static function notWithinNamespace(string $requestHandlerClass, string $controllersBaseNamespace):self
     {
         return new self(sprintf("The PSR-7 request handler %s is not within the base namespace '%s'.",
-            $requestHandlerClass, $controllersBaseNamespace), self::CODE_NOT_WITHIN_NAMESPACE);
-    }
-
-    /**
-     * @param string $requestHandlerClass
-     * @return RouterException
-     */
-    public static function noRouteFound(string $requestHandlerClass):self
-    {
-        return new self(sprintf("No route is available for the request handler '%s'.", $requestHandlerClass),
-            self::CODE_NO_ROUTE_FOUND);
-    }
-
-    /**
-     * @param mixed|object|string $item
-     * @return RouterException
-     */
-    public static function notARouter($item):self
-    {
-        return new self(sprintf("The item '%s' is not a router. All routers must implement '%s'.",
-            is_object($item) ? get_class($item) : (string)$item, RouterInterface::class),
-            self::CODE_NOT_A_ROUTER);
-    }
-
-    /**
-     * @param mixed|object|string $item
-     * @return RouterException
-     */
-    public static function notAMiddleware($item):self
-    {
-        return new self(sprintf("The item '%s' is not a middleware. All middleware must implement '%s'.",
-            is_object($item) ? get_class($item) : (string)$item, MiddlewareInterface::class),
-            self::CODE_NOT_A_MIDDLEWARE);
-    }
-
-    /**
-     * @param string $requestHandlerClass
-     * @return RouterException
-     */
-    public static function emptyRoute(string $requestHandlerClass):self
-    {
-        return new self(sprintf("The route to the request handler '%s' can not be empty.", $requestHandlerClass),
-            self::CODE_EMPTY_ROUTE);
-    }
-
-    /**
-     * @param mixed $route
-     * @param string $requestHandlerClass
-     * @return RouterException
-     */
-    public static function invalidRoute($route, string $requestHandlerClass):self
-    {
-        return new self(sprintf("The route '%s' to the request handler '%s' is not valid.",
-            (string)$route, $requestHandlerClass), self::CODE_INVALID_ROUTE);
+            $requestHandlerClass, $controllersBaseNamespace),
+            self::CODE_NOT_WITHIN_NAMESPACE);
     }
 }
