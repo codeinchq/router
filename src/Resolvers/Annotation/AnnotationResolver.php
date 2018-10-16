fe42a90ed11d79b0abc94439febad6b3b2338e9c
Joan Fabrégat <joan@codeinc.fr>
2018-10-16 11:55:03 +0200

    adds the possibiltiy to use a custom annotation reader

--- src/Resolvers/Annotation/AnnotationResolver.php
+++ src/Resolvers/Annotation/AnnotationResolver.php
@@ -21,9 +21,9 @@
 declare(strict_types=1);
 namespace CodeInc\Router\Resolvers\Annotation;
 use CodeInc\DirectoryClassesIterator\RecursiveDirectoryClassesIterator;
-use CodeInc\Router\Resolvers\HandlerResolverInterface;
 use CodeInc\Router\Resolvers\StaticHandlerResolver;
 use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\Common\Annotations\Reader;
 
 
 /**
@@ -40,28 +40,34 @@ class AnnotationResolver extends StaticHandlerResolver
     private $routePrefix;
 
     /**
+     * @var Reader
+     */
+    private $annotationReader;
+
+    /**
      * AnnotationResolver constructor.
      *
      * @param string $routePrefix
+     * @param Reader|null $annotationReader
+     * @throws \Doctrine\Common\Annotations\AnnotationException
      */
-    public function __construct(string $routePrefix = '')
+    public function __construct(string $routePrefix = '', ?Reader $annotationReader = null)
     {
         parent::__construct();
         $this->routePrefix = $routePrefix;
+        $this->annotationReader = $annotationReader ?? new AnnotationReader();
     }
 
     /**
-     * Adds all the handler in a directory having the
+     * Adds all the handler in a directory having the annotation @Routable.
      *
      * @param string $dirPath
-     * @throws \Doctrine\Common\Annotations\AnnotationException
      */
     public function addDirectory(string $dirPath):void
     {
-        $reader = new AnnotationReader();
         foreach (new RecursiveDirectoryClassesIterator($dirPath) as $class) {
             /** @var Routable $annotation */
-            if ($annotation = $reader->getClassAnnotation($class, Routable::class)) {
+            if ($annotation = $this->annotationReader->getClassAnnotation($class, Routable::class)) {
                 $this->addRoute($this->routePrefix.$annotation->route, $class->getName());
                 if ($annotation->altRoutes) {
                     foreach ($annotation->altRoutes as $route) {
